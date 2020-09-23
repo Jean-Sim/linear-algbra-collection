7054a229d42c2eeb9935d2edc59be04c637376a9
Jean-Sim <59151535+Jean-Sim@users.noreply.github.com>
2020-09-23 20:32:43 +0200

    Update dot_product.py

--- individual modules/dot_product.py
+++ individual modules/dot_product.py
@@ -1,5 +1,6 @@
 def dot_product(vector_1, vector_2):
     dot_product = 0
+    # defines end variable  
     for index in range(len(vector_1)):
         try:
             dot_product += vector_1[index] * vector_2[index]
