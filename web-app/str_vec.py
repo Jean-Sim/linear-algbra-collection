c634a793694e9010eacb7695d38d7a6625095d64
Jean-Sim <59151535+Jean-Sim@users.noreply.github.com>
2020-12-24 22:42:23 +0100

    Update str_vec.py

--- web-app/str_vec.py
+++ web-app/str_vec.py
@@ -1,9 +1,9 @@
 def string_vector(lis):
     lis = lis.strip("[")
     lis = lis.strip("]")
-    # removes the unnecessary brackets from string
+    # removes all unnecessary brackets from string
     lis = lis.split(",")
-    # splits string into an array 
+    # splits the string into an array 
     for element in range(len(lis)):
         lis[element] = float(lis[element])
         # turns string in to numbers
