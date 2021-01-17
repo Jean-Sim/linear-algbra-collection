076dc61b9ecf181a8da4f72d089077b8bfc0e49a
Jean-Sim <59151535+Jean-Sim@users.noreply.github.com>
2021-01-17 21:12:44 +0100

    Update str_vec.py

--- web-app/str_vec.py
+++ web-app/str_vec.py
@@ -1,9 +1,9 @@
 def string_vector(lis):
     lis = lis.strip("[")
     lis = lis.strip("]")
-    # removes all unnecessary brackets from string
+    # removes all unnecessary brackets from string 
     lis = lis.split(",")
-    # splits the string into an array 
+    # splits the vector in string form into an array 
     for element in range(len(lis)):
         lis[element] = float(lis[element])
         # turns the number string in to numbers
